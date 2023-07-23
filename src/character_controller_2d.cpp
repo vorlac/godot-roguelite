3849f14835b10fcb88cc3cbba3bc8e9b4507a1ad
sal <sal@orlac.io>
2023-07-23 17:12:59 -0400

    cleanup

--- src/character_controller_2d.cpp
+++ src/character_controller_2d.cpp
@@ -13,38 +13,23 @@ namespace godot
 
         ClassDB::bind_method(D_METHOD("get_speed"), &CharacterController2D::get_speed);
         ClassDB::bind_method(D_METHOD("set_speed", "speed"), &CharacterController2D::set_speed);
-        PropertyInfo speed_property_info{ Variant::VECTOR2, "speed", PROPERTY_HINT_RANGE, "0,2000,1" };
-        ADD_PROPERTY(speed_property_info, "set_speed", "get_speed");
-
         ClassDB::bind_method(D_METHOD("get_acceleration"), &CharacterController2D::get_acceleration);
         ClassDB::bind_method(D_METHOD("set_acceleration", "acceleration"), &CharacterController2D::set_acceleration);
-        PropertyInfo accel_property_info{ Variant::VECTOR2, "acceleration", PROPERTY_HINT_RANGE, "0,2000,1" };
-        ADD_PROPERTY(accel_property_info, "set_acceleration", "get_acceleration");
-
         ClassDB::bind_method(D_METHOD("get_friction"), &CharacterController2D::get_friction);
         ClassDB::bind_method(D_METHOD("set_friction", "friction"), &CharacterController2D::set_friction);
+
+        PropertyInfo speed_property_info{ Variant::VECTOR2, "speed", PROPERTY_HINT_RANGE, "0,2000,1" };
+        PropertyInfo accel_property_info{ Variant::VECTOR2, "acceleration", PROPERTY_HINT_RANGE, "0,2000,1" };
         PropertyInfo friction_property_info{ Variant::FLOAT, "friction", PROPERTY_HINT_RANGE, "0,1,0.01" };
+
+        ADD_PROPERTY(speed_property_info, "set_speed", "get_speed");
+        ADD_PROPERTY(accel_property_info, "set_acceleration", "get_acceleration");
         ADD_PROPERTY(friction_property_info, "set_friction", "get_friction");
     }
 
     void CharacterController2D::_ready()
     {
         m_velocity = { 0.0, 0.0 };
-    }
-
-    void CharacterController2D::_unhandled_input(const Ref<InputEvent>& event)
-    {
-        /*   Input* input = Input::get_singleton();
-           real_t vert_input = input->get_axis("ui_up", "ui_down");
-           real_t horiz_input = input->get_axis("ui_left", "ui_right");
-
-           Vector2 target_velocity{ Math::is_zero_approx(horiz_input) ? 0.0f : horiz_input,
-                                    Math::is_zero_approx(vert_input) ? 0.0f : vert_input };
-
-           Vector2 temp1{ m_velocity.lerp(target_velocity, m_friction) };
-           Vector2 temp2{ temp1.clamp({ -1.0, -1.0 }, { 1.0, 1.0 }) };
-           m_velocity = temp2;
-           UtilityFunctions::print(m_velocity);*/
     }
 
     void CharacterController2D::_physics_process(double delta_time)
@@ -56,30 +41,16 @@ namespace godot
         Vector2 target_velocity{ Math::is_zero_approx(horiz_input) ? 0.0f : horiz_input,
                                  Math::is_zero_approx(vert_input) ? 0.0f : vert_input };
 
-        Vector2 temp1{ m_velocity.lerp(target_velocity, m_friction) };
-        Vector2 temp2{ temp1.clamp({ -1.0, -1.0 }, { 1.0, 1.0 }) };
-        m_velocity = temp2;
-        auto movement_offst{ m_velocity * m_target_speed * delta_time };
-        this->set_position(this->get_position() + movement_offst);
+        m_velocity = { m_velocity.lerp(target_velocity, m_friction) };
+        m_velocity = { m_velocity.clamp({ -1.0, -1.0 }, { 1.0, 1.0 }) };
 
-        /*Node::_physics_process(delta_time);
-        Input* input = Input::get_singleton();*/
+        auto movement_offset{ m_velocity * m_target_speed * delta_time };
+        this->set_position(this->get_position() + movement_offset);
     }
 
     void CharacterController2D::_process(double delta_time)
     {
-        // Node::_process(delta_time);
+        Node::_process(delta_time);
         // process_movement(delta_time);
-    }
-
-    void CharacterController2D::process_movement(double delta_time)
-    {
-        /*const Vector2 movement{ Input::get_singleton()->get_vector("ui_left", "ui_right", "ui_up", "ui_down") };
-
-        m_velocity += movement.is_zero_approx() ? -m_friction : movement * m_acceleration;
-        m_velocity *= m_target_speed * delta_time;
-
-        auto clamped_velocity = m_velocity.clamp(m_min_speed, m_max_speed);
-        this->set_position(this->get_position() + clamped_velocity);*/
     }
 }
--- src/character_controller_2d.hpp
+++ src/character_controller_2d.hpp
@@ -1,10 +1,10 @@
 #pragma once
-// clang-format off
 
 #include <godot_cpp/classes/input_event.hpp>
 #include <godot_cpp/classes/node2d.hpp>
 #include <godot_cpp/classes/ref.hpp>
 #include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
 
 namespace godot
 {
@@ -16,39 +16,38 @@ namespace godot
         void _ready() override;
         void _process(double delta_time) override;
         void _physics_process(double delta_time) override;
-        virtual void _unhandled_input(const Ref<godot::InputEvent>& event) override;
 
-        // virtual void _process(double delta);
-        // virtual void _physics_process(double delta);
-        // virtual void _enter_tree();
-        // virtual void _exit_tree();
-        // virtual void _ready();
-        // virtual void _input(const Ref<InputEvent>& event);
-        // virtual void _shortcut_input(const Ref<InputEvent>& event);
-        // virtual void _unhandled_input(const Ref<InputEvent>& event);
-        // virtual void _unhandled_key_input(const Ref<InputEvent>& event);
-        // virtual PackedStringArray _get_configuration_warnings() const;
+        // void _process(double delta);
+        // void _physics_process(double delta);
+        // void _enter_tree();
+        // void _exit_tree();
+        // void _ready();
+        // void _input(const Ref<InputEvent>& event);
+        // void _shortcut_input(const Ref<InputEvent>& event);
+        // void _unhandled_input(const Ref<InputEvent>& event);
+        // void _unhandled_key_input(const Ref<InputEvent>& event);
+        // PackedStringArray _get_configuration_warnings() const;
 
         static void _bind_methods();
-        void process_movement(double delta_time);
 
-        // movement property getters
+        // clang-format off
         inline Vector2 get_speed() const { return m_target_speed; }
         inline Vector2 get_max_speed() const { return m_max_speed; }
         inline Vector2 get_acceleration() const { return m_acceleration; }
         inline real_t get_friction() const { return m_friction; }
 
-        // movement property setters
         inline void set_speed(const Vector2& speed) { m_target_speed = speed; }
         inline void set_max_speed(const Vector2& max_speed) { m_max_speed = max_speed; }
         inline void set_acceleration(const Vector2& accel) { m_acceleration = accel; }
         inline void set_friction(const real_t friction) { m_friction = friction; }
 
+        // clang-format on
+
     private:
         // Elapsed time in ms since last update
         double m_elapsed_time{ 0.0 };
         // Rate of decelleration when movement inputs aren't active (units/ms)
-        real_t m_friction{ 0.25 };
+        real_t m_friction{ 0.1 };
         // Rate of accelleration (units/ms)
         Vector2 m_acceleration{ 0.1, 0.1 };
         // Target character walk speed (units/ms)
@@ -61,7 +60,4 @@ namespace godot
         // was just applied to character position (units)
         Vector2 m_velocity{ 0.0, 0.0 };
     };
-
 }
-
-// clang-format on
