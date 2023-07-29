86c75cdf8e118883afae93590ee8799175ef3c6c
sal <sal@orlac.io>
2023-07-29 04:36:58 -0400

    bunch of cleanup, Character is now a CharacterBody2D

--- src/character.cpp
+++ src/character.cpp
@@ -1,56 +1,77 @@
 #include "character.hpp"
 
+#include "gdextension_interface.h"
+
+#include <array>
+#include <functional>
+#include <ranges>
+#include <tuple>
+#include <utility>
 #include <godot_cpp/classes/collision_shape2d.hpp>
 #include <godot_cpp/classes/engine.hpp>
 #include <godot_cpp/classes/input.hpp>
 #include <godot_cpp/classes/input_event.hpp>
+#include <godot_cpp/classes/input_map.hpp>
+#include <godot_cpp/classes/ref.hpp>
+#include <godot_cpp/classes/shape2d.hpp>
 #include <godot_cpp/classes/viewport.hpp>
+#include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/core/math.hpp>
+#include <godot_cpp/core/property_info.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
 
 namespace godot
 {
     void Character::_bind_methods()
     {
-        UtilityFunctions::print(FUNCTION_STR);
+        utils::print(FUNCTION_STR);
 
-        ClassDB::bind_method(D_METHOD("get_speed"), &Character::get_speed);
-        ClassDB::bind_method(D_METHOD("set_speed", "speed"), &Character::set_speed);
-        ClassDB::bind_method(D_METHOD("get_acceleration"), &Character::get_acceleration);
-        ClassDB::bind_method(D_METHOD("set_acceleration", "acceleration"), &Character::set_acceleration);
-        ClassDB::bind_method(D_METHOD("get_friction"), &Character::get_friction);
-        ClassDB::bind_method(D_METHOD("set_friction", "friction"), &Character::set_friction);
-        ClassDB::bind_method(D_METHOD("get_rotation_speed"), &Character::get_rotation_speed);
-        ClassDB::bind_method(D_METHOD("set_rotation_speed", "rotation_speed"), &Character::set_rotation_speed);
+        constexpr std::array METHOD_BINDINGS = {
+            std::tuple(
+                std::pair("get_movement_speed", &Character::get_movement_speed),
+                std::pair("set_movement_speed", &Character::set_movement_speed),
+                std::pair("move_speed", Variant::FLOAT)
+            ),
+            std::tuple(
+                std::pair("get_movement_friction", &Character::get_movement_friction),
+                std::pair("set_movement_friction", &Character::set_movement_friction),
+                std::pair("move_friction", Variant::FLOAT)
+            ),
+            std::tuple(
+                std::pair("get_rotation_speed", &Character::get_rotation_speed),
+                std::pair("set_rotation_speed", &Character::set_rotation_speed),
+                std::pair("rotation_speed", Variant::FLOAT)
+            )
+        };
 
-        PropertyInfo speed_property_info{ Variant::VECTOR2, "speed", PROPERTY_HINT_RANGE, "0,2000,1" };
-        PropertyInfo accel_property_info{ Variant::VECTOR2, "acceleration", PROPERTY_HINT_RANGE, "0,2000,1" };
-        PropertyInfo friction_property_info{ Variant::FLOAT, "friction", PROPERTY_HINT_RANGE, "0,1,0.01" };
-        PropertyInfo rot_speed_property_info{ Variant::FLOAT, "rotation_speed", PROPERTY_HINT_RANGE, "0,50,0.01" };
-
-        ADD_PROPERTY(speed_property_info, "set_speed", "get_speed");
-        ADD_PROPERTY(accel_property_info, "set_acceleration", "get_acceleration");
-        ADD_PROPERTY(friction_property_info, "set_friction", "get_friction");
-        ADD_PROPERTY(rot_speed_property_info, "set_rotation_speed", "get_rotation_speed");
+        for (auto&& binding : METHOD_BINDINGS)
+        {
+            auto&& [getter_info, setter_info, properties] = std::move(binding);
+            auto&& [getter_method_name, getter_callable] = std::move(getter_info);
+            auto&& [setter_method_name, setter_callable] = std::move(setter_info);
+            auto&& [property_name, property_type] = std::move(properties);
+            PropertyInfo binding_prop_info{ property_type, property_name };
+            ClassDB::bind_method(D_METHOD(getter_method_name), getter_callable);
+            ClassDB::bind_method(D_METHOD(setter_method_name, property_name), setter_callable);
+            ADD_PROPERTY(binding_prop_info, setter_method_name, getter_method_name);
+        }
     }
 
     void Character::_ready()
     {
-        UtilityFunctions::print(FUNCTION_STR);
-        m_velocity = { 0.0, 0.0 };
-        CollisionObject2D* collision_obj = memnew(CollisionObject2D);
-        collision_obj->set_transform(this->get_transform());
-        this->add_child(collision_obj);
+        this->set_motion_mode(MotionMode::MOTION_MODE_FLOATING);
+        InputMap* const input_map{ InputMap::get_singleton() };
+        input_map->load_from_project_settings();
     }
 
     void Character::_enter_tree()
     {
-        UtilityFunctions::print(FUNCTION_STR);
+        utils::print(FUNCTION_STR);
     }
 
     void Character::_exit_tree()
     {
-        // can't use UtilityFunctions::print here, crashes
+        //
     }
 
     void Character::_input(const Ref<InputEvent>& event)
@@ -70,27 +91,37 @@ namespace godot
 
     void Character::_unhandled_input(const Ref<InputEvent>& event)
     {
-        // TODO: fix... doesn't handle deadzones properly
-        const String event_type{ event->get_class() };
-        switch (m_input_mode)
-        {
-            default:
-                [[fallthrough]];
-            case InputMode::MouseAndKeyboard:
-            {
-                // InputEventJoypadMotion, InputEventJoypadButton
-                if (event_type.begins_with("InputEventJoypad"))
-                    m_input_mode = InputMode::Controller;
-                break;
-            }
-            case InputMode::Controller:
-            {
-                // InputEventMouseMotion, InputEventMouseButton, InputEventKey
-                if (event_type.begins_with("InputEventMouse") || event_type.begins_with("InputEventKey"))
-                    m_input_mode = InputMode::MouseAndKeyboard;
-                break;
-            }
-        }
+        //
+    }
+
+    double Character::get_movement_speed() const
+    {
+        return m_movement_speed;
+    }
+
+    double Character::get_movement_friction() const
+    {
+        return m_movement_friction;
+    }
+
+    double Character::get_rotation_speed() const
+    {
+        return m_rotation_speed;
+    }
+
+    void Character::set_movement_speed(const double move_speed)
+    {
+        m_movement_speed = move_speed;
+    }
+
+    void Character::set_movement_friction(const double move_friction)
+    {
+        m_movement_friction = move_friction;
+    }
+
+    void Character::set_rotation_speed(const double rotation_speed)
+    {
+        m_rotation_speed = rotation_speed;
     }
 
     void Character::_physics_process(double delta_time)
@@ -100,22 +131,33 @@ namespace godot
 
     void Character::_process(double delta_time)
     {
-        // called every frame
         Input* const input{ Input::get_singleton() };
-        this->process_movement_input(input, delta_time);
-        this->process_rotation_input(input, delta_time);
+        if (input != nullptr)
+        {
+            m_elapsed_time += delta_time;
+            if constexpr (RL_DEBUG)
+                input->set_block_signals(false);
+
+            this->process_movement_input(input, delta_time);
+            this->process_rotation_input(input, delta_time);
+
+            Point2 mouse_pos{ this->get_global_mouse_position() };
+            if (this->get_viewport_rect().has_point(mouse_pos))
+                input->flush_buffered_events();
+
+            if constexpr (RL_DEBUG)
+                input->set_block_signals(true);
+        }
     }
 
     void Character::process_movement_input(Input* const input, double delta_time)
     {
-        Vector2 raw_movement_input{ input->get_vector("ui_left", "ui_right", "ui_up", "ui_down") };
-        Vector2 target_velocity = raw_movement_input.is_zero_approx() ? Vector2{ 0, 0 } : raw_movement_input;
-
-        m_velocity = m_velocity.lerp(target_velocity, m_friction);
-        m_velocity = m_velocity.clamp({ -1.0, -1.0 }, { 1.0, 1.0 });
-
-        auto movement_offset{ m_velocity * m_target_speed * delta_time };
-        this->set_position(this->get_position() + movement_offset);
+        Vector2 velocity{ input->get_vector("move_left", "move_right", "move_up", "move_down") };
+        velocity = this->get_velocity().lerp(velocity, m_movement_friction * delta_time);
+        velocity = velocity.clamp({ -1.0, -1.0 }, { 1.0, 1.0 });
+        this->translate(velocity * this->get_movement_speed() * delta_time);
+        this->set_velocity(velocity);
+        this->move_and_slide();
     }
 
     Character::InputMode Character::get_input_mode(Input* const input)
@@ -131,10 +173,14 @@ namespace godot
                 {
                     uint32_t controller_id = controllers.front();
 
-                    float rotation_x_axis = input->get_joy_axis(controller_id, JoyAxis::JOY_AXIS_RIGHT_X);
-                    float rotation_y_axis = input->get_joy_axis(controller_id, JoyAxis::JOY_AXIS_RIGHT_Y);
-                    float movement_x_axis = input->get_joy_axis(controller_id, JoyAxis::JOY_AXIS_LEFT_X);
-                    float movement_y_axis = input->get_joy_axis(controller_id, JoyAxis::JOY_AXIS_LEFT_Y);
+                    float rotation_x_axis =
+                        input->get_joy_axis(controller_id, JoyAxis::JOY_AXIS_RIGHT_X);
+                    float rotation_y_axis =
+                        input->get_joy_axis(controller_id, JoyAxis::JOY_AXIS_RIGHT_Y);
+                    float movement_x_axis =
+                        input->get_joy_axis(controller_id, JoyAxis::JOY_AXIS_LEFT_X);
+                    float movement_y_axis =
+                        input->get_joy_axis(controller_id, JoyAxis::JOY_AXIS_LEFT_Y);
 
                     auto deadzone = 0.15;
                     rotation_x_axis = Math::abs(rotation_x_axis) < deadzone ? 0.0 : rotation_x_axis;
@@ -142,8 +188,10 @@ namespace godot
                     movement_x_axis = Math::abs(rotation_x_axis) < deadzone ? 0.0 : rotation_x_axis;
                     movement_y_axis = Math::abs(rotation_y_axis) < deadzone ? 0.0 : rotation_y_axis;
 
-                    if (!Math::is_zero_approx(rotation_x_axis) || !Math::is_zero_approx(rotation_y_axis) ||
-                        !Math::is_zero_approx(movement_x_axis) || !Math::is_zero_approx(movement_y_axis))
+                    if (!Math::is_zero_approx(rotation_x_axis) ||
+                        !Math::is_zero_approx(rotation_y_axis) ||
+                        !Math::is_zero_approx(movement_x_axis) ||
+                        !Math::is_zero_approx(movement_y_axis))
                         m_input_mode = InputMode::Controller;
                 }
                 break;
@@ -161,45 +209,40 @@ namespace godot
 
     void Character::process_rotation_input(Input* const input, const double delta_time)
     {
+        // double rotation_angle{ this->get_rotation() };
         switch (this->get_input_mode(input))
         {
             default:
                 [[fallthrough]];
             case InputMode::MouseAndKeyboard:
             {
-                Point2 mouse_position{ this->get_viewport()->get_mouse_position() };
-                m_rotation_angle = this->get_position().angle_to_point(this->get_global_mouse_position()) +
-                                   Math::deg_to_rad(90.0);
+                Vector2 rotation_direction{ this->get_global_mouse_position() -
+                                            this->get_global_position() };
+                m_rotation_angle = rotation_direction.angle() + Math::deg_to_rad(90.0);
                 break;
             }
             case InputMode::Controller:
             {
-                TypedArray<int32_t> controllers{ input->get_connected_joypads() };
+                TypedArray<int32_t> controllers{ std::move(input->get_connected_joypads()) };
                 if (controllers.is_empty())
-                    UtilityFunctions::printerr("Input Mode = Controller, but no controllers detected");
+                    utils::printerr("InputMode = Controller, but no controllers detected");
                 else
                 {
-                    uint32_t controller_id = controllers.front();
-
-                    float x_axis = input->get_joy_axis(controller_id, JoyAxis::JOY_AXIS_RIGHT_X);
-                    float y_axis = input->get_joy_axis(controller_id, JoyAxis::JOY_AXIS_RIGHT_Y);
-
-                    auto deadzone = 0.15;
-                    x_axis = Math::abs(x_axis) < deadzone ? 0.0 : x_axis;
-                    y_axis = Math::abs(y_axis) < deadzone ? 0.0 : y_axis;
-
-                    if (!Math::is_zero_approx(x_axis) || !Math::is_zero_approx(y_axis))
-                    {
-                        const Vector2 target_rotation{ -y_axis, x_axis };
-                        m_rotation_angle = Vector2{ 0, 0 }.angle_to_point(target_rotation);
-                    }
+                    const uint32_t controller_id = controllers.front();
+                    const Vector2 target_rotation = input->get_vector(
+                        "rotate_left", "rotate_right", "rotate_up", "rotate_down"
+                    );
+                    if (!target_rotation.is_zero_approx())
+                        m_rotation_angle = Vector2{ 0, 0 }.angle_to_point(target_rotation) +
+                                           Math::deg_to_rad(90.0);
                 }
-
                 break;
             }
         }
 
-        double rotation{ Math::lerp_angle(this->get_rotation(), m_rotation_angle, m_rotation_speed * delta_time) };
-        this->set_rotation(rotation);
+        const double smoothed_rotation_angle =
+            Math::lerp_angle(this->get_rotation(), m_rotation_angle, m_rotation_speed * delta_time);
+
+        this->set_rotation(smoothed_rotation_angle);
     }
 }
--- src/character.hpp
+++ src/character.hpp
@@ -3,15 +3,21 @@
 #include <godot_cpp/classes/character_body2d.hpp>
 #include <godot_cpp/classes/input_event.hpp>
 #include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/math.hpp>
 
 namespace godot
 {
     class Input;
+    class InputMap;
+    class UtilityFunctions;
 
     class Character : public CharacterBody2D
     {
-        GDCLASS(Character, Node2D)
+        GDCLASS(Character, CharacterBody2D);
+
     private:
+        using utils = godot::UtilityFunctions;
+
         enum class InputMode
         {
             MouseAndKeyboard,
@@ -28,47 +34,34 @@ namespace godot
         void _unhandled_input(const Ref<InputEvent>& event) override;
         void _unhandled_key_input(const Ref<InputEvent>& event) override;
         void _shortcut_input(const Ref<InputEvent>& event) override;
-
         static void _bind_methods();
 
-        // clang-format off
-        inline Vector2 get_speed() const { return m_target_speed; }
-        inline Vector2 get_max_speed() const { return m_max_speed; }
-        inline Vector2 get_acceleration() const { return m_acceleration; }
-        inline real_t get_friction() const { return m_friction; }
-        inline real_t get_rotation_speed() const { return m_rotation_speed; }
+        [[nodiscard]] double get_movement_speed() const;
+        [[nodiscard]] double get_movement_friction() const;
+        [[nodiscard]] double get_rotation_speed() const;
+        void set_movement_speed(const double move_speed);
+        void set_movement_friction(const double move_friction);
+        void set_rotation_speed(const double rotation_speed);
 
-        inline void set_speed(const Vector2& speed) { m_target_speed = speed; }
-        inline void set_max_speed(const Vector2& max_speed) { m_max_speed = max_speed; }
-        inline void set_acceleration(const Vector2& accel) { m_acceleration = accel; }
-        inline void set_friction(const real_t friction) { m_friction = friction; }
-        inline void set_rotation_speed(const real_t rot_speed) { m_rotation_speed = rot_speed; }
-
-        // clang-format on
     protected:
         InputMode get_input_mode(Input* const input);
         void process_movement_input(Input* input, double delta_time);
         void process_rotation_input(Input* input, double delta_time);
 
     private:
-        // Elapsed time in ms since last update
-        double m_elapsed_time{ 0.0 };
-        // Rate of decelleration when movement inputs aren't active (units/ms)
-        real_t m_friction{ 0.1 };
-        // Rate of accelleration (units/ms)
-        Vector2 m_acceleration{ 0.1, 0.1 };
-        // Target character walk speed (units/ms)
-        Vector2 m_target_speed{ 500.0, 500.0 };
-        // Maximum allowable character speed (units/ms)
-        Vector2 m_min_speed{ 0.0, 0.0 };
-        // Maximum allowable character speed (units/ms)
-        Vector2 m_max_speed{ 1500.0f, 1500.0f };
-        // The current movement distance/direction that
-        // was just applied to character position (units)
-        Vector2 m_velocity{ 0.0, 0.0 };
-        // Vector2 m_target_rotation{ 0.0, 0.0 };
-        real_t m_rotation_speed{ 5.0 };
-        double m_rotation_angle{ 0.0 };
+        // active input mode for character controls
         InputMode m_input_mode{ InputMode::MouseAndKeyboard };
+        // Rate of acceleration/deceleration (unit/s/s)
+        double m_movement_friction{ 5.0 };
+        // Rate of acceleration/deceleration (unit/s/s)
+        double m_rotation_speed{ 5.0 };
+        // target movement speed (units/s)
+        double m_movement_speed{ 500.0 };
+        // target facing angle (radians)
+        double m_rotation_angle{ 0.0 };
+        // elapsed runtime (seconds)
+        double m_elapsed_time{ 0.0 };
+        // TODO: break this out into an editor tool
+        constexpr static inline bool RL_DEBUG{ true };
     };
 }
