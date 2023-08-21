9bfdd7f1da884a796acb748afa9f88bc7884bc65
sal <sal@orlac.io>
2023-08-20 23:47:33 -0400

    vcpkg / 3rd party library integration

--- src/ui/main_interface_diag.cpp
+++ src/ui/main_interface_diag.cpp
@@ -28,46 +28,9 @@
 
 namespace rl
 {
-    static constexpr inline auto profiler_cap = [](void* this_ptr, const godot::String& cmd,
-                                                   const godot::Array& data, bool& captured) {
-        return reinterpret_cast<MainInterfaceDiag*>(this_ptr)->message_callback(cmd, data,
-                                                                                captured);
-    };
-
     void MainInterfaceDiag::test(int notification)
     {
     }
-
-    // static void RedirectStreamRL(const char* p_file_name, const char* p_mode, FILE* p_cpp_stream,
-    //                              const DWORD p_std_handle)
-    // {
-    //     const HANDLE h_existing = GetStdHandle(p_std_handle);
-    //     if (h_existing != INVALID_HANDLE_VALUE)
-    //     {  // Redirect only if attached console have a valid handle.
-    //         const HANDLE h_cpp = reinterpret_cast<HANDLE>(_get_osfhandle(_fileno(p_cpp_stream)));
-    //         if (h_cpp == INVALID_HANDLE_VALUE)
-    //         {  // Redirect only if it's not already redirected to the pipe or file.
-    //             FILE* fp = p_cpp_stream;
-    //             freopen_s(&fp, p_file_name, p_mode, p_cpp_stream);  // Redirect stream.
-    //             setvbuf(p_cpp_stream, nullptr, _IONBF, 0);          // Disable stream buffering.
-    //         }
-    //     }
-    // }
-
-    // enum ErrorHandlerType
-    // {
-    //     ERR_HANDLER_ERROR,
-    //     ERR_HANDLER_WARNING,
-    //     ERR_HANDLER_SCRIPT,
-    //     ERR_HANDLER_SHADER,
-    // };
-
-    // extern void (*_print_func)(godot::String);
-    // typedef void (*PrintHandlerFunc)(void*, const godot::String& p_string, bool p_error,
-    //                                  bool p_rich);
-    // typedef void (*ErrorHandlerFunc)(void*, const char*, const char*, int p_line, const char*,
-    //                                  const char*, bool p_editor_notify,
-    //                                  rl::ErrorHandlerType p_type);
 
     MainInterfaceDiag::MainInterfaceDiag()
     {
@@ -77,49 +40,10 @@ namespace rl
     MainInterfaceDiag::~MainInterfaceDiag()
     {
         stdoutRedirect.Stop();
-        m_is_active = false;
-        if (m_console_write_thread != nullptr)
-            m_console_write_thread->join();
-    }
-
-    void MainInterfaceDiag::message_callback(const godot::String& cmd, const godot::Array& data,
-                                             bool& captured)
-    {
-        m_console->add_text("cmd=" + cmd + " data=" + godot::Variant(data).stringify() +
-                            " cap=" + (captured ? "true" : "false") + "\n");
     }
 
     void MainInterfaceDiag::_ready()
     {
-        static const auto asdf = [&](int notification) {
-            while (m_is_active)
-            {
-                {
-                    std::cout << "__notification__ => " << std::to_string(notification)
-                              << std::endl;
-                    log::info("log::[info]: " + godot::itos(notification));
-                    log::error("log::[error] : " + godot::itos(notification));
-                    log::warning("log::[warn] Notification: " + godot::itos(notification));
-                    printf("[printf]: notification = %d", notification);
-
-                    stdoutRedirect.Stop();
-                    // std::scoped_lock<std::mutex> lock{ m_lock };
-                    char buffer[512] = { 0 };
-                    size_t read_bytes = stdoutRedirect.GetBuffer(buffer, 512);
-                    if (read_bytes > 0)
-                    {
-                        std::string_view out{ buffer, read_bytes };
-                        m_console->add_text(out.data());
-                    }
-                }
-                using namespace std::chrono_literals;
-                std::this_thread::sleep_for(0.1s);
-            }
-        };
-
-        // m_is_active = true;
-        //  m_console_write_thread = new std::thread(asdf, 0);
-
         if (m_console == nullptr)
         {
             auto scene_root{ scene::tree::root_node(this) };
@@ -127,23 +51,13 @@ namespace rl
                 scene_root->find_child("ConsolePanel", true, false));
             debug::assert(m_console != nullptr);
         }
-        //    std::streambuf* stdout_stream{ std::cout.rdbuf() };
-        //    std::cout.rdbuf(stdout_strstm.rdbuf());
-
-        //    debug::engine::debugger::register_logger(
-        //        SignalConnection("rl_message_handler", &handle_messages));
-
-        //    // CreateConsole();
-        //}
     }
 
     void MainInterfaceDiag::_notification(int notification)
     {
-        // TODO: fixme
-        // stdio gets piped to console in new UI, but internal logging APIs don't.
-        // need to hook into the C io file/stream handles so it can all be intercepted
         if (m_console != nullptr)
         {
+            fmt::print("asdf");
             std::cout << "__notification__ => " << std::to_string(notification) << std::endl;
             log::info("log::[info]: " + godot::itos(notification));
             log::error("log::[error] : " + godot::itos(notification));
@@ -159,30 +73,5 @@ namespace rl
                 m_console->newline();
             }
         }
-
-        // TODO: remove
-        // stdout_strstm.flush();
-        // std::string test = stdout_strstm.str();
-        // godot::String str{ test.c_str() };
-        // if (!str.is_empty() && m_console != nullptr)
-        //{
-        //    // m_console->add_text(str);
-        //    // m_console->newline();
-        //}
-        // switch (notification)
-        //{
-        //    case NOTIFICATION_DRAW:
-        //    {
-        //        if (!str.is_empty())
-        //        {
-        //            // m_console->append_text(str);
-        //            // m_console->queue_redraw();
-        //        }
-        //        break;
-        //    }
-        //    default:
-        //        break;
-        //}
     }
-
 }
--- src/ui/main_interface_diag.hpp
+++ src/ui/main_interface_diag.hpp
@@ -10,6 +10,7 @@
 #endif
 
 #include <atomic>
+#include <fmt/format.h>
 #include <mutex>
 #include <ostream>
 #include <sstream>
@@ -23,7 +24,6 @@
 
 namespace rl
 {
-
     static void CreateConsole()
     {
         if (!AllocConsole())
@@ -42,53 +42,7 @@ namespace rl
         std::clog.clear();
         std::cerr.clear();
         std::cin.clear();
-
-        //// std::wcout, std::wclog, std::wcerr, std::wcin
-        // HANDLE hConOut = CreateFile(_T("CONOUT$"), GENERIC_READ | GENERIC_WRITE,
-        //                             FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING,
-        //                             FILE_ATTRIBUTE_NORMAL, NULL);
-        // HANDLE hConIn = CreateFile(_T("CONIN$"), GENERIC_READ | GENERIC_WRITE,
-        //                            FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING,
-        //                            FILE_ATTRIBUTE_NORMAL, NULL);
-        // SetStdHandle(STD_OUTPUT_HANDLE, hConOut);
-        // SetStdHandle(STD_ERROR_HANDLE, hConOut);
-        // SetStdHandle(STD_INPUT_HANDLE, hConIn);
-        // std::wcout.clear();
-        // std::wclog.clear();
-        // std::wcerr.clear();
-        // std::wcin.clear();
     }
-
-    // static void RedirectStreamRL(const char* p_file_name, const char* p_mode, FILE* p_cpp_stream,
-    //                              const DWORD p_std_handle)
-    // {
-    //     const HANDLE h_existing = GetStdHandle(p_std_handle);
-    //     if (h_existing != INVALID_HANDLE_VALUE)
-    //     {  // Redirect only if attached console have a valid handle.
-    //         const HANDLE h_cpp = reinterpret_cast<HANDLE>(_get_osfhandle(_fileno(p_cpp_stream)));
-    //         if (h_cpp == INVALID_HANDLE_VALUE)
-    //         {  // Redirect only if it's not already redirected to the pipe or file.
-    //             FILE* fp = p_cpp_stream;
-    //             freopen_s(&fp, p_file_name, p_mode, p_cpp_stream);  // Redirect stream.
-    //             setvbuf(p_cpp_stream, nullptr, _IONBF, 0);          // Disable stream buffering.
-    //         }
-    //     }
-    // }
-
-    // enum ErrorHandlerType
-    // {
-    //     ERR_HANDLER_ERROR,
-    //     ERR_HANDLER_WARNING,
-    //     ERR_HANDLER_SCRIPT,
-    //     ERR_HANDLER_SHADER,
-    // };
-
-    // extern void (*_print_func)(godot::String);
-    // typedef void (*PrintHandlerFunc)(void*, const godot::String& p_string, bool p_error,
-    //                                  bool p_rich);
-    // typedef void (*ErrorHandlerFunc)(void*, const char*, const char*, int p_line, const char*,
-    //                                  const char*, bool p_editor_notify,
-    //                                  rl::ErrorHandlerType p_type);
 
     class MainInterfaceDiag : public godot::Panel
     {
@@ -119,5 +73,4 @@ namespace rl
         godot::RichTextLabel* m_console{ nullptr };
         StdOutRedirect stdoutRedirect{ 512 };
     };
-
 }
