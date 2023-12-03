79ee7ab4d7ca510f2c86416d70f5b2e81c1bf5c7
sal <sal@orlac.io>
2023-12-03 04:35:49 -0500

    another test

--- cmake/ccache.cmake
+++ cmake/ccache.cmake
@@ -1,22 +1,19 @@
 # SPDX-License-Identifier: Unlicense
 
 # See: https://crascit.com/2016/04/09/using-ccache-with-cmake/
-find_program( CCACHE_PROGRAM ccache )
+find_program(CCACHE_PROGRAM ccache)
 
-if ( CCACHE_PROGRAM )
-    # get version information
+if (CCACHE_PROGRAM)
     execute_process(
         COMMAND "${CCACHE_PROGRAM}" --version
         OUTPUT_VARIABLE CCACHE_VERSION
     )
 
-    string( REGEX MATCH "[^\r\n]*" CCACHE_VERSION ${CCACHE_VERSION} )
+    string(REGEX MATCH "[^\r\n]*" CCACHE_VERSION ${CCACHE_VERSION})
+    message("Using ccache: ${CCACHE_PROGRAM} (${CCACHE_VERSION})")
 
-    message( STATUS "Using ccache: ${CCACHE_PROGRAM} (${CCACHE_VERSION})" )
+    set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_PROGRAM}")
+    set(CMAKE_C_COMPILER_LAUNCHER "${CCACHE_PROGRAM}")
 
-    # Turn on ccache for all targets
-    set( CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_PROGRAM}" )
-    set( CMAKE_C_COMPILER_LAUNCHER "${CCACHE_PROGRAM}" )
-
-    unset( CCACHE_VERSION )
+    unset(CCACHE_VERSION)
 endif()
--- src/util/debug.hpp
+++ src/util/debug.hpp
@@ -23,32 +23,5 @@ namespace rl::inline utils
             MainMenuInit,
             All
         };
-
-        // using optval_t = std::pair<Option, bool>;
-        // static constexpr inline auto DebugSettings{
-        //     std::to_array<std::type_identity_t<optval_t>>({
-        //         { Option::All, false },
-        //         { Option::RootProcess, true },
-        //         { Option::RootPhysics, true },
-        //         { Option::RootViewport, true },
-        //         { Option::RootInputs, true },
-        //         { Option::LevelProcess, true },
-        //         { Option::LevelLoad, true },
-        //         { Option::LevelUnload, true },
-        //         { Option::MainMenuInit, true },
-        //     }),
-        // };
-
-        // static constexpr inline bool is_enabled(const Option& diag_type)
-        // {
-        //     for (auto&& [opt, val] : DebugSettings)
-        //     {
-        //         if (opt == Option::All && val)
-        //             return true;
-        //         if (opt == diag_type)
-        //             return val;
-        //     }
-        //     return false;
-        // }
     }
 }
