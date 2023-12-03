012b7774bfd0d9e488e64d6b53b26f25a8b37750
sal <sal@orlac.io>
2023-12-03 03:47:02 -0500

    change test

--- src/util/debug.hpp
+++ src/util/debug.hpp
@@ -24,31 +24,31 @@ namespace rl::inline utils
             All
         };
 
-        using optval_t = std::pair<Option, bool>;
-        static constexpr inline auto DebugSettings{
-            std::to_array<std::type_identity_t<optval_t>>({
-                { Option::All, false },
-                { Option::RootProcess, true },
-                { Option::RootPhysics, true },
-                { Option::RootViewport, true },
-                { Option::RootInputs, true },
-                { Option::LevelProcess, true },
-                { Option::LevelLoad, true },
-                { Option::LevelUnload, true },
-                { Option::MainMenuInit, true },
-            }),
-        };
+        // using optval_t = std::pair<Option, bool>;
+        // static constexpr inline auto DebugSettings{
+        //     std::to_array<std::type_identity_t<optval_t>>({
+        //         { Option::All, false },
+        //         { Option::RootProcess, true },
+        //         { Option::RootPhysics, true },
+        //         { Option::RootViewport, true },
+        //         { Option::RootInputs, true },
+        //         { Option::LevelProcess, true },
+        //         { Option::LevelLoad, true },
+        //         { Option::LevelUnload, true },
+        //         { Option::MainMenuInit, true },
+        //     }),
+        // };
 
-        static constexpr inline bool is_enabled(const Option& diag_type)
-        {
-            for (auto&& [opt, val] : DebugSettings)
-            {
-                if (opt == Option::All && val)
-                    return true;
-                if (opt == diag_type)
-                    return val;
-            }
-            return false;
-        }
+        // static constexpr inline bool is_enabled(const Option& diag_type)
+        // {
+        //     for (auto&& [opt, val] : DebugSettings)
+        //     {
+        //         if (opt == Option::All && val)
+        //             return true;
+        //         if (opt == diag_type)
+        //             return val;
+        //     }
+        //     return false;
+        // }
     }
 }
