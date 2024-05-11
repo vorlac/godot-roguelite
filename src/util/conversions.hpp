c7489ad3825a2494305162d31c36e5c619c069d5
Mangonels <dylancalaflatham@gmail.com>
2024-05-11 18:30:10 +0200

    gd_str_conv

--- src/util/conversions.hpp
+++ src/util/conversions.hpp
@@ -14,10 +14,11 @@
 
 namespace rl::inline utils
 {
+	/** Converts std string types to godot::String at compile time */
     template <typename TStr>
-    struct GDStrConv
+    struct gd_str_conv
     {
-        explicit constexpr GDStrConv(TStr&& s)
+        explicit constexpr gd_str_conv(TStr&& s)
             : m_str{ std::move(s) }
         {
         }
